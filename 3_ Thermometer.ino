ac08c402a0f6ec3d2a66c2c38a34a1af78a05a19
tk0103 <ninntenntarou@gmail.com>
2018-06-15 00:39:29 +0900

    Create 3_ Thermometer.ino

---
+++ 3_ Thermometer.ino
@@ -0,0 +1,28 @@
+#include <LiquidCrystal.h>
+LiquidCrystal lcd( 4, 6, 10, 11, 12, 13 );
+
+void setup() {
+     Serial.begin(9600) ;      // パソコン(ArduinoIDE)とシリアル通信の準備を行う
+     lcd.begin( 16, 2 );
+}
+
+void loop() {
+    lcd.clear();
+    lcd.setCursor(0, 0);
+  
+     float tmp1,tmp2,ans;
+     tmp1 = 0.0;
+     tmp2 = 0.0;
+
+     for (int i=0; i <= 4; i++){
+        tmp1 = analogRead(0);
+        tmp1 = tmp1 * 5 / 1024.0 *100;
+        tmp2 = tmp2+ tmp1;
+        delay(30);
+     }
+
+     ans = tmp2 /5;
+     lcd.print(ans);
+     delay(5000) ;              
+tmp2 = 0;
+}
